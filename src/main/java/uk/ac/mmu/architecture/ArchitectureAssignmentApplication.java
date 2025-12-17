9c58832b4c9e8e0672623be58e160cc859880a9d
James Nash <jamesnash007@hotmail.com>
2025-12-17 10:35:44 +0000

    initial commit

---
+++ src/main/java/uk/ac/mmu/architecture/ArchitectureAssignmentApplication.java
@@ -0,0 +1,13 @@
+package uk.ac.mmu.architecture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArchitectureAssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArchitectureAssignmentApplication.class, args);
+	}
+
+}
