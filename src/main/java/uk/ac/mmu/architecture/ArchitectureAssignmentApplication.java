4283d48e344fd4d459666548511ab56ac327489d
James Nash <jamesnash007@hotmail.com>
2025-12-18 00:10:20 +0000

    new shit

--- src/main/java/uk/ac/mmu/architecture/ArchitectureAssignmentApplication.java
+++ src/main/java/uk/ac/mmu/architecture/ArchitectureAssignmentApplication.java
@@ -9,31 +9,27 @@ import uk.ac.mmu.architecture.applicationcode.Boards.TwoPlayerBoardFactory;
 import uk.ac.mmu.architecture.applicationcode.Games.ReplayGameFacade;
 import uk.ac.mmu.architecture.applicationcode.Rulesets.*;
 
-
 @SpringBootApplication
 public class ArchitectureAssignmentApplication {
-	public static void main(String[] args) {
-		SpringApplication.run(ArchitectureAssignmentApplication.class, args);
-		ArchitectureAssignmentApplication app = new ArchitectureAssignmentApplication();
-		app.runGames();
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(ArchitectureAssignmentApplication.class, args);
+    ArchitectureAssignmentApplication app = new ArchitectureAssignmentApplication();
+    app.runGames();
+  }
 
-	private void runGames() {
-		BoardFactory twoPlayerBoardFactory = new TwoPlayerBoardFactory();
-		Board twoPlayerBoard = twoPlayerBoardFactory.create(
-				twoPlayerBoardFactory.generatePlayers(),
-				twoPlayerBoardFactory.generateTrack(),
-				twoPlayerBoardFactory.generateTails()
-		);
+  private void runGames() {
+    BoardFactory twoPlayerBoardFactory = new TwoPlayerBoardFactory();
+    Board twoPlayerBoard =
+        twoPlayerBoardFactory.create(
+            twoPlayerBoardFactory.generatePlayers(),
+            twoPlayerBoardFactory.generateTrack(),
+            twoPlayerBoardFactory.generateTails());
 
-		int[] rolls = {6, 6, 6, 6, 3, 4, 3, 4};
+    int[] rolls = {6, 6, 6, 6, 3, 4, 3, 4};
 
-		ReplayGameFacade game = new ReplayGameFacade(
-				rolls,
-				new StandardHitCondition(),
-				new StandardWinCondition(),
-				twoPlayerBoard
-		);
-		game.start();
-	}
+    ReplayGameFacade replayGame =
+        new ReplayGameFacade(
+            rolls, new StandardHitCondition(), new StandardWinCondition(), twoPlayerBoard);
+    replayGame.start();
+  }
 }
